6ab98ce07e4282ddffbb9e5605728731c8283fa5
mirandashaekelley <mkelley@student.neumont.edu>
2022-01-14 08:57:59 -0700

    starting routes/endpoints

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,16 @@
+
+const runCrudOperations = require('./routes/routes.js');
 const express = require("express");
 const bodyParser = require("body-parser");
 const expressSession = require("express-session");
 const routes = require("./routes/routes");
-const cors = require("cors");
+//const cors = require("cors");
 const path = require("path");
 
 const app = express();
+
+
+app.listen(3000, function(){
+    console.log('listening on 3000')
+});
+

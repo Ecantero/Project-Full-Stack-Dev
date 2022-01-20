3f1d62c85d7c53f56bdfd97a627747dcffc9b545
mirandashaekelley <mkelley@student.neumont.edu>
2022-01-20 08:19:28 -0700

    \\

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -13,26 +13,67 @@ const client = new MongoClient(url);
 
 const app = express();
 
-//connecting to cluster, access database,  and closing it
-async function main(){
-    try {
-        //connect to cluster
+app.get('/users', async (req,res) => {
+    try{
         await client.connect();
-
-        //make db calls
-        await listDatabases(client);
-        const findResult = await collection.find({}).toArray();
-        console.log('Found documents =>', findResult);
-
-
-    }catch(e){
-        console.error(e);
-    }finally{
+        const fetchedUsers = await collection.find({}).toArray();
+        res.json(fetchedUsers);
+    
+    } catch(error){
+        res.json(error);
+    } finally{
         await client.close();
     }
-}
+})
 
-main().catch(console.error);
+//connecting to cluster, access database,  and closing it
+// async function main(){
+//     try {
+//         //connect to cluster
+//         await client.connect();
+
+//         //make db calls
+//         await listDatabases(client);
+//         const findResult = await collection.find({}).toArray();
+//         console.log('Found documents =>', findResult);
+
+
+//     }catch(e){
+//         console.error(e);
+//     }finally{
+//         await client.close();
+//     }
+// }
+
+//main().catch(console.error);
+
+
+
+// app.post('/users', function(req,res){
+//     client.connect();
+//     const findUsers = collection.find({}).toArray();
+//     const body = JSON.stringify(findUsers);
+//     res.json(body);
+//     client.close();
+// });
+
+// app.post('/login', (req, res) =>{
+//     //res.json({status: 'ok', data:'coming soon'});
+//     let {email, password} = req.body;
+//     email = email.trim();
+//     password = password.trim();
+
+//     if(email == "" || password == ""){
+//         res.json({
+//             status: "FAILED",
+//             message: "Empty credentials suppilied"
+//         })
+//     } else {
+        
+//     }
+// });
+
+
 
 
 async function listDatabases(client){
@@ -45,15 +86,11 @@ async function listDatabases(client){
 
 
 app.get("/", function(req, res){
-    console.log('GET request');
+    res.send('GET request');
     res.end();
 });
 
 
-app.get('/users', function(req,res){
-   // res.send({type: 'GET'});
-    
-});
 
 app.listen(process.env.port || 4000, function(){
     console.log('now listening for requests');

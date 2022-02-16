539c2826cff483b155433b0059f5e652e63872e2
mirandashaekelley <mkelley@student.neumont.edu>
2022-02-16 13:23:51 -0700

    navigation :D

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { ReactDOM } from 'react';
 import { BrowserRouter as Router, useRoutes, Route, Routes } from "react-router-dom";
 import Homepage from './Homepage';
+import  Login  from './Login';
+import  Navigation  from './Navigation';
 
 
 const App = () => {
@@ -10,8 +12,14 @@ const App = () => {
    {
      path: '/',
      element: <Homepage />,
+   },
+   {
+  
+        path: "/login",
+        element: <Login />
+     
    }
- ]);
+  ]);
  return element;
  
 }
@@ -19,6 +27,7 @@ const App = () => {
 const AppWrapper = () =>{
   return(
     <Router>
+      <Navigation/>
       <App/>
     </Router>
   )
--- react-front-end/src/Homepage.js
+++ react-front-end/src/Homepage.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Homepage = () => {
   return (
+    
     <div>movie planet</div>
   )
 }

f1b972f2537fe38dbb9c4d3790999d4e6f75f464
Dylan <60489717+DRoberts919@users.noreply.github.com>
2022-03-02 09:36:10 -0700

    id is gotten

--- react-front-end/src/components/Details.js
+++ react-front-end/src/components/Details.js
@@ -1,103 +1,98 @@
-import React, { useEffect, useState } from 'react'
-import Detail from './Detail';
-import Movie from './Movie';
-import axios from 'axios';
-import useFetch,{fetch} from "./useFetch";
+import React, { useEffect, useState } from "react";
+import Detail from "./Detail";
+import Movie from "./Movie";
+import axios from "axios";
+import useFetch, { fetch } from "./useFetch";
+import { useSearchParams } from "react-router-dom";
 // import './Homepage.css';
 
 const windowPathName = window.location.pathname;
 const id = windowPathName.slice(9);
-const API_KEY = 'f669645cad8fbe1526a40b2deee8a49e';
-const FEATURED_API = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
-const IMG_API = "https://image.tmdb.org/t/p/w200/";
 
 function Details() {
-    
-//    const { movies, isLoaded, error, refetch} = useFetch(FEATURED_API);
-   const [movies,setMovieDetails] = useState(null);
+  //    const { movies, isLoaded, error, refetch} = useFetch(FEATURED_API);
+  const [movies, setMovieDetails] = useState(null);
+  const [params] = useSearchParams();
+  let id = params.get("movieID");
+  console.log(id);
 
-    useEffect(() =>{
-        getData()
-           // refetch();
-        },[])
+  const API_KEY = "f669645cad8fbe1526a40b2deee8a49e";
+  const FEATURED_API = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
+  const IMG_API = "https://image.tmdb.org/t/p/w200/";
 
-        // console.log(newFetch)
+  useEffect(() => {
+    getData();
+    // refetch();
+  }, []);
 
-//    if(isLoaded) return <h1>LOADING</h1>;
-//    if(error){ 
-//        //refetch();
-//        console.log(error)
-//     };
+  // console.log(newFetch)
 
-    const getData = async ()=>{
-        let data = await fetch(FEATURED_API)
-        console.log(data.data)
-        setMovieDetails(data.data)
-    }
-  
-   return(
-       <div className='movieDetails'>
-           <div className='movieDetailsContainer'>
-                <div className='movieTitle'>{movies?.title}</div>
-                <div className='movieGenres'>
-                    <div>{movies?.genres[0].name} </div>
-                    <div>{movies?.genres[1].name}</div>
-                    <div>{movies?.genres[2].name}</div> 
-                </div>
-                <div className='movieTagline'>"{movies?.tagline}"</div>
-                <div className='movieOverview'>
-                    <img src={IMG_API + movies?.poster_path} alt={movies?.title}/> 
-                    <div>{movies?.overview}</div>
-                </div> 
+  //    if(isLoaded) return <h1>LOADING</h1>;
+  //    if(error){
+  //        //refetch();
+  //        console.log(error)
+  //     };
 
-                <div>Actors</div>
-                <div>{movies?.actor}</div>
-                {/* <div onClick={refetch}>click me</div> */}
-            </div>
+  const getData = async () => {
+    let data = await fetch(FEATURED_API);
+    console.log(data.data);
+    setMovieDetails(data.data);
+  };
+
+  return (
+    <div className='movieDetails'>
+      <div className='movieDetailsContainer'>
+        <div className='movieTitle'>{movies?.title}</div>
+        <div className='movieGenres'>
+          <div>{movies?.genres[0].name} </div>
+          <div>{movies?.genres[1].name}</div>
+          <div>{movies?.genres[2].name}</div>
         </div>
-   );
+        <div className='movieTagline'>"{movies?.tagline}"</div>
+        <div className='movieOverview'>
+          <img src={IMG_API + movies?.poster_path} alt={movies?.title} />
+          <div>{movies?.overview}</div>
+        </div>
 
-  
+        <div>Actors</div>
+        <div>{movies?.actor}</div>
+        {/* <div onClick={refetch}>click me</div> */}
+      </div>
+    </div>
+  );
 
-   
-    
-    // Note:  the empty array means the useEffect will run like componentDidMount()
-          
-    // useEffect(() =>{
-    //     axios
-    //     .get(FEATURED_API)
-    //     .then(res => {
-    //         console.log(res)
-    //         setMovies(res.data)
-    //         console.log(movies)
-    //     })
-    //     .catch(err =>{
-    //         console.log(err)
-    //     })
-    // }, [])
+  // Note:  the empty array means the useEffect will run like componentDidMount()
 
-    // useEffect(() =>{
-       
-    //     fetch(FEATURED_API)
-    //     .then((res) => res.json())
-    //     .then((data) => {
-    //         console.log(data);
-    //         setMovies(data.results);
-           
-    //     });
+  // useEffect(() =>{
+  //     axios
+  //     .get(FEATURED_API)
+  //     .then(res => {
+  //         console.log(res)
+  //         setMovies(res.data)
+  //         console.log(movies)
+  //     })
+  //     .catch(err =>{
+  //         console.log(err)
+  //     })
+  // }, [])
 
-    //     console.log(movies);
-    // }, []);
-    
+  // useEffect(() =>{
 
-    
-        // return <div className='moviesList'>
-            
-        //         <div>hi</div>
-            
-        // </div>;
-  
+  //     fetch(FEATURED_API)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //         console.log(data);
+  //         setMovies(data.results);
+
+  //     });
+
+  //     console.log(movies);
+  // }, []);
+
+  // return <div className='moviesList'>
+
+  //         <div>hi</div>
+
+  // </div>;
 }
-export default Details
-
-
+export default Details;
